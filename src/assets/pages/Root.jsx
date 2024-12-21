34b680e0987400d6ea61a5711933df2b8462fd79
Raiyan27 <abdullahalraiyan4@gmail.com>
2024-12-21 18:33:25 +0600

    first commit

---
+++ src/assets/pages/Root.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Header from "../components/Header";
+
+const Root = () => {
+  return (
+    <>
+      <Header />
+      <h1>Root</h1>
+    </>
+  );
+};
+
+export default Root;
